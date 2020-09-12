02285e2c28077f532b82ab0c11626f112db13fe5
Topology1225 <e0804s.haruki@gmail.com>
2020-09-12 17:12:17 +0900

    CNN, resnetの実装

--- classification/src/models/ResNet18/model.py
+++ classification/src/models/ResNet18/model.py
@@ -1,18 +1,37 @@
 import torch
 import torch.nn as nn
-import torchvision
+import torchvision 
+import torchvision.models as models
 
-class ResNet18(nn.modules):
+class ResNet18(nn.Module): 
     def __init__(self):
-        pass
+        super().__init__()
+        self.name = "resnet18"
+        self.epoch = 0
+        self.build()
 
-    def forward(self):
-        pass
+    def build(self):
+        resnet18 = models.resnet18() 
+        self.resnet18 = resnet18
+
+    def forward(self, x):
+        x = self.resnet18(x)
+        return x
+        
 
     def save(self):
         pass
 
     def load(self):
         pass
+
+
+if __name__=="__main__":
+    model = ResNet18() 
+    x = torch.ones((1,3,256,256))
+    x = model(x)
+    breakpoint()
+
+
 
     
