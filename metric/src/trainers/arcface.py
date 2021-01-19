f389506dce2ad00511bd77a0e35fbb6af6307f84
Topology1225 <e0804s.haruki@gmail.com>
2021-01-19 23:55:52 +0900

    arcfaceの完成, _describeの追加2

--- metric/src/trainers/arcface.py
+++ metric/src/trainers/arcface.py
@@ -1,3 +1,5 @@
+import os
+from collections import defaultdict
 import math
 from threading import Thread
 
@@ -7,9 +9,10 @@ import torch.nn.functional as F
 from torch.cuda import amp
 from torch.nn import Parameter
 from tqdm import tqdm
+import numpy as np
 
 from trainers.BaseTrainer import BaseTrainer
-from utils import setup_logger
+from utils import setup_logger, save2json
 
 logger = setup_logger(__name__)
 
@@ -86,7 +89,7 @@ class ArcfaceLoss(nn.Module) :
     
     def forward(self, x, labels): 
         arc_x = self.arcface(x, labels) 
-        x = F.softmax(arc_x, dim=1)   
+        # x = F.softmax(arc_x, dim=1)   
         loss = self.cel(x, labels)
         return loss
     
@@ -97,9 +100,8 @@ class ArcfaceLoss(nn.Module) :
         
 
 class ArcfaceTrainer(BaseTrainer):
-    l_train = dict()
-    l_valid = dict()
-    
+    l_train = defaultdict(dict)
+    l_valid = defaultdict(dict)
     def __init__(self, config, dset_config):
         super().__init__()
         self.config = config
@@ -110,7 +112,7 @@ class ArcfaceTrainer(BaseTrainer):
     def _constructor(self):
         pass
 
-    def _train(self, epochs, batch_size, dataset, valid_dataset, model):
+    def _train(self, epochs, batch_size, dataset, valid_dataset):
         # arcfaceのパラメタの追加
         self.optimizer.add_param_group(dict(params=self.criterion.parameters()))
 
@@ -129,53 +131,74 @@ class ArcfaceTrainer(BaseTrainer):
         logger.info(('\n' + '%10s' * 3) % ('Epoch', 'gpu_mem', 'loss')) 
         
         for e in range(epochs): 
-            model.train()
-            self.criterion.train()
-
+            self.model.train()
             pbar = enumerate(train_dloader)
-            pbar = tqdm(pbar, total=len(dataset)/batch_size)
-            t_sum_loss = 0
-            v_sum_loss = 0
+            pbar = tqdm(pbar, total=math.ceil(len(dataset)/batch_size))
+            t_sum_loss = list()
+            v_sum_loss = list()
+            
             for i, (x, labels) in pbar:  
                 with amp.autocast(enabled=self.cuda):  
                     x = x.to(self.device)
                     labels = labels.to(self.device)
-                    output = model(x)
+                    output = self.model(x)
                     loss = self.criterion(output, labels) 
                     self._set_updater(loss)
 
                     logger.debug(f"\n {loss.detach().cpu().item()}")
-                    logger.debug(f"\n {model.state_dict()['module.layers.0.fc.weight']}")
-
-                    t_sum_loss += loss.detach().cpu().item()
+                    t_sum_loss.append(loss.detach().cpu().item())
 
                 ## string
                 mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
                 s = \
-                    ('%10s' + '%10.4s'*2)%(('%g/%g' % (e, epochs - 1)), mem, t_sum_loss/(i+1)) 
+                    ('%10s' + '%10.4s'*2)%(('%g/%g' % (e, epochs - 1)), mem, np.mean(t_sum_loss)) 
                 pbar.set_description(s)
 
-            self.l_train[e] = dict(
-                        loss=t_sum_loss/(i+1)
-                )
+            self.l_train["loss"][e] = np.mean(t_sum_loss) 
 
             with torch.no_grad():
-                model.eval()
-                self.criterion.eval()
+                self.model.eval()
                 pbar = enumerate(valid_dloader)
-                pbar = tqdm(pbar, total=len(valid_dataset))
-                for x, labels in valid_dloader:
+                pbar = tqdm(pbar, total=math.ceil(len(valid_dataset)/batch_size))
+                for i, (x, labels) in pbar:
                     with amp.autocast(enabled=self.cuda):  
                         x = x.to(self.device)
                         labels = labels.to(self.device)
-                        output = model(x)
+                        output = self.model(x)
                         loss = self.criterion(output, labels) 
-                        v_sum_loss += loss.detach().cpu().item()
+                        v_sum_loss.append(loss.detach().cpu().item()) 
+                    mem = '%.3gG' % (torch.cuda.memory_reserved() / 1E9 if torch.cuda.is_available() else 0)  # (GB)
+                    s = \
+                        ('%10s' + '%10.4s'*2)%(('%g/%g' % (e, epochs - 1)), mem, np.mean(v_sum_loss)) 
+                    pbar.set_description(s)
 
-            self.l_valid[e] = dict(
-                loss=v_sum_loss/(i+1)
-            )   
+            self.l_valid["loss"][e] = np.mean(v_sum_loss) 
 
+    def _describe(self):
+        x_dict = dict(
+            train=self.l_train,
+            valid=self.l_valid
+        )
+        self._plot_loss(x_dict)
+
+        # to json 
+        json_file_name = os.path.join(self.config.result_dir, "result.json")
+        save2json(x_dict, json_file_name)
+
+        # save model
+        self._save_model(epoch=self.config.train.epoch, name="last")
+
+    def _save_model(self, epoch, name=None):
+        if name is None:
+            name = f"arcface-{epoch}"
+        ckpt = dict(
+            epoch=epoch,
+            model=self.model.cpu().state_dict(),
+            criterion=self.criterion.cpu().state_dict(),
+            optimizer=self.optimizer.state_dict()
+        )
+        pth_file = os.path.join(self.config.result_dir, "weights", f"{name}.pth") 
+        torch.save(ckpt, pth_file)
 
     def __repr__(self) -> str:
         return super().__repr__()
