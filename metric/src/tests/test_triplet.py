5d20e1dac445b5721e95c3549118520be3387e92
Topology1225 <e0804s.haruki@gmail.com>
2021-01-19 17:46:10 +0900

    test用のyamlの追加

---
+++ metric/src/tests/test_triplet.py
@@ -0,0 +1,30 @@
+from attrdict import AttrDict
+
+from trainers import get_trainer
+from dataset import get_dataset
+from models import get_model 
+from utils import read_yaml
+
+def test_build_triplet():
+    config = read_yaml("./tests/config/triplet.yaml")
+    dset_config = read_yaml("./tests/config/mnist.yaml")
+
+    dset = get_dataset(
+        config, dset_config, mode="train"
+    )
+    valid_dset = get_dataset (
+        config, dset_config, mode="valid"
+    )
+    model = get_model(
+        config, dset_config
+    )
+
+    trainer = get_trainer(
+        config, dset_config
+    )
+    # trainer.train(
+    #     dataset=dset,
+    #     valid_dataset=valid_dset,
+    #     model=model
+    # )
+
