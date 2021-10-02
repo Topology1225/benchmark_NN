7ec3cdcb4f9444cee13c87b9715bad83dbec3dab
Topology1225 <satoharu@opt-amp4.imi.kyushu-u.ac.jp>
2021-10-02 16:33:02 +0900

    add hub abc true

--- metric/src/models/resnet.py
+++ metric/src/models/resnet.py
@@ -5,9 +5,10 @@ from utils import initilize
 
 from utils.logging import setup_logger
 
+
 logger = setup_logger(__name__)
 
-
+torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
 class ResNet(torch.nn.Module):
     def __init__(self, config, dset_config):
         super().__init__()
@@ -18,12 +19,12 @@ class ResNet(torch.nn.Module):
     def _constructor(self):
         assert not self.config.loss.name is None
         nc = self.config.loss.params.num_dim
-
         model = torch.hub.load(
             self.config.model.version,
             self.config.model.name,
             pretrained=self.config.model.pretrained,
         )
+
         fc = torch.nn.Linear(in_features=1000, out_features=nc)
 
         initilize(self.config, [fc])
